04c046267764f3ebe52aaeeea6d1371ab0e5f046
DEV <dev@DEVs-MacBook-Air.local>
2025-04-25 08:45:09 -0500

    Reverse_Linked_List

---
+++ Reverse_Linked_List.py
@@ -0,0 +1,25 @@
+"""
+    Given the head of a singly linked list, reverse the list, and return the reversed list.
+"""
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+      if not head :
+        return head
+      else:  
+        curr = head
+        prev = None  
+        while curr:
+            next = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next
+        self.head = prev  
+      return prev
+
+
+        
