827cf06965bf42716e08a802a31450c4d0804de7
DEV <dev@DEVs-MacBook-Air.local>
2025-05-06 11:11:48 -0500

    Intersection of Two Linked Lists

--- Intersection_of_Two_Linked_Lists.py
+++ Intersection_of_Two_Linked_Lists.py
@@ -4,11 +4,12 @@
     For example, the following two linked lists begin to intersect at node c1:
     
 """
+from typing import Optional
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
@@ -19,3 +20,8 @@ class Solution:
             tempA = tempA.next if tempA else headB
             tempB = tempB.next if tempB else headA
         return tempA
+    
+listA = [4,1,8,4,5]
+listB = [5,6,1,8,4,5]
+s = Solution()
+print(s.getIntersectionNode(listA,listB)) 
